c5ef5894a203bec131427d152bf39a586cf8a1b3
Shoaib.kat.ali <101383726+WALI59021@users.noreply.github.com>
2022-11-16 15:31:01 +0500

    Add files via upload

---
+++ Codealigned Project/main/default/lwc/accountdetailscomponent/accountdetailscomponent.js
@@ -0,0 +1,26 @@
+import { LightningElement } from 'lwc';
+import accountdetailsByAccountId from '@salesforce/apex/Accountdetailsclass.accountdetailsByAccountId';
+
+const columns =[
+    { label: 'Account Name', fieldName: 'Name' },
+    { label: 'Rating ', fieldName: 'Rating', type:'Picklist' },
+    { label: 'Industry', fieldName: 'Industry', type:'Picklist' },
+    { label: 'Close Date', fieldName: 'Phone' , type:'Phone'},
+    { label: 'Amount', fieldName: 'AnnualRevenue', type: 'currency',cellAttributes: { alignment: 'left' } },
+    { label: 'Owner ID', fieldName: 'OwnerId' },
+    {
+        type: 'action',
+        typeAttributes: { rowActions: actions },
+    }
+]
+
+
+export default class Accountdetailscomponent extends LightningElement {
+
+
+    columns = columns;
+    @api accountId;
+    @wire(accountdetailsByAccountId,{accountId:'$accountId'}) Account;
+
+
+}
